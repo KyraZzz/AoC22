b3170f44eaa74e7063cb9a43971853c4a01a1969
KyraZzz <yz709@cam.ac.uk>
2024-07-23 21:35:15 +0100

    done day6 challenge lo

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,34 +1,12 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::HashSet;
+
+use itertools::Itertools;
 
 fn decode_line(line: &str) -> Option<usize> {
-    let chars = line.chars().collect::<Vec<_>>();
-    let mut signals = VecDeque::new();
-    let mut signal_set: HashSet<char> = HashSet::new();
-    let mut char_idx = 0;
-    while char_idx < chars.len() {
-        if signal_set.len() == 14 {
-            return Some(char_idx);
-        }
-
-        let char = chars[char_idx];
-        if signal_set.contains(&char) {
-            while !signals.is_empty() {
-                let pop_char = signals.pop_front().unwrap();
-                signal_set.remove(&pop_char);
-                if pop_char != char {
-                    continue;
-                } else {
-                    break;
-                }
-            }
-        }
-        signals.push_back(char);
-        signal_set.insert(char);
-
-        char_idx += 1;
-    }
-
-    None
+    line.as_bytes()
+        .windows(4)
+        .position(|window| window.iter().unique().count() == 4)
+        .map(|pos| pos + 4)
 }
 
 fn main() {
